b8fd11595d60faa618317e3882eaaed1fec62d07
frup0 <111578022+frup0@users.noreply.github.com>
2022-09-11 17:23:45 +0300

    1.0.1.2

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -2,27 +2,20 @@ extends KinematicBody2D
 
 export (int) var speed = 200
 
-onready var imagePlayer = get_node("AnimatedSprite")
-
 var velocity = Vector2()
 
 func get_input():
 	velocity = Vector2()
-	if Input.is_action_pressed("right"):
+	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
-	if Input.is_action_pressed("left"):
+	if Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
-	if Input.is_action_pressed("down"):
+	if Input.is_action_pressed("ui_down"):
 		velocity.y += 1
-	if Input.is_action_pressed("up"):
+	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
 	velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
 	get_input()
 	velocity = move_and_slide(velocity)
-
-if velocity.x > 0:
-	imagePlayer.flip_h = true
-elif velocity.x < 0:
-	imagePlayer.flip_h = false
